32036ecd8ddc2d53069c02df47f9cf5c63a37425
v.sulzhin <chijovka@gmail.com>
2020-07-08 15:05:24 +0300

    Контроль на максимальное количество алмазов во FreeSpin Контроль на максимальное количество Additions FreeSpin

--- vendor/avior/game-core/src/Instructions/WorkersInstructions/LogicWorkerInstructions/LogicWorkerFreeSpinInstruction.php
+++ vendor/avior/game-core/src/Instructions/WorkersInstructions/LogicWorkerInstructions/LogicWorkerFreeSpinInstruction.php
@@ -32,9 +32,19 @@ class LogicWorkerFreeSpinInstruction implements IInstruction
                 $dataPool->logicData->lineBet * $dataPool->logicData->linesInGame
             );
 
-            $dataPool->logicData->table = $toolsPool->logicTools->tableTool->getRandomTable(
-                $currentPercentages
-            );
+            do {
+                $dataPool->logicData->table = $toolsPool->logicTools->tableTool->getRandomTable(
+                    $currentPercentages
+                );
+
+                // исключение возможности выпадения больше $max_count алмазов в FreeSpin
+                $check = $this->excludeMaxDiamandCountOnTable($dataPool->logicData->table, $dataPool->logicData->multiplier, 27);
+
+                if($check) {
+                    //проверка на $max_count Addition FreeSpin'a
+                    $check = $this->excludeMaxAdditionFreeSpins($dataPool->logicData->table, $dataPool->logicData->countOfMovesInFeatureGame, 2);
+                }
+            } while (!$check);
         } else {
             $dataPool->logicData->table = $dataPool->systemData->tablePreset;
         }
@@ -44,6 +54,72 @@ class LogicWorkerFreeSpinInstruction implements IInstruction
         //$dataPool->logicData->table = [2,1,3,5,0,6,7,8,9,4,2,3,4,5,6];
 
         return $dataPool;
+    }
+
+    /**
+     * Исключение возможности выпадения больше $max_count алмазов в FreeSpin
+     *
+     * @param array $table
+     * @param $multiplier
+     * @param int $max_count
+     *
+     * @return bool
+     */
+    protected function excludeMaxDiamandCountOnTable(array $table, int $multiplier, int $max_count)
+    {
+        $check = true;
+
+        $bonusSymbolCount = 0;
+        foreach ($table as $item) {
+            if ($item === 0) {
+                $bonusSymbolCount += 1;
+            }
+        }
+
+        // -2 т.к. начинаем с множителя 2
+        if ($bonusSymbolCount > 0) {
+            if (($multiplier - 2 + $bonusSymbolCount) > $max_count) {
+                $check = false;
+            }
+        }
+
+        return $check;
+    }
+
+    /**
+     * Проверка на $max_count Addition FreeSpin'a
+     *
+     * @param array $table
+     * @param int $count_of_moves_in_feature_game
+     * @param int $max_count
+     *
+     * @return bool
+     */
+    protected function excludeMaxAdditionFreeSpins(array $table, int $count_of_moves_in_feature_game, int $max_count)
+    {
+        $check = true;
+
+        $bonusSymbolCount = 0;
+        foreach ($table as $item) {
+            if ($item === 0) {
+                $bonusSymbolCount += 1;
+            }
+        }
+
+        $coinSymbolCount = 0;
+        foreach ($table as $item) {
+            if ($item === 10) {
+                $coinSymbolCount += 1;
+            }
+        }
+
+        if(($bonusSymbolCount + $coinSymbolCount) >= 3) {
+            if ((($count_of_moves_in_feature_game - 12) / 12) >= $max_count) {
+                $check = false;
+            }
+        }
+
+        return $check;
     }
 
     /**
