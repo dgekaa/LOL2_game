182dad4c1c5f7098f6feaec05e48a35b93aadc3a
Zhidko Vladislav <vladislav.zhidko@zimad.com>
2020-08-01 18:43:10 +0300

    update top labels position

--- public/games/lifeOfLuxury2/desktop.game1.js
+++ public/games/lifeOfLuxury2/desktop.game1.js
@@ -325,7 +325,7 @@ function game1() {
         }
 
         function animTopLabel(img) {
-            game.add.tween(topLabel).to({y: topLabel.position.y + 120}, 400, "Linear", true).onComplete.add(function () {
+            game.add.tween(topLabel).to({y: img === 'top_label_1' ? topLabel.position.y + 120 : topLabel.position.y + 103}, 400, "Linear", true).onComplete.add(function () {
                 changeImgTopLabel(img)
                 game.add.tween(topLabel).to({y: img === 'top_label_1' ? topLabel.position.y - 120 : topLabel.position.y - 103}, 400, "Linear", true).onComplete.add(function () {
                     changeImgTopLabel(img)
