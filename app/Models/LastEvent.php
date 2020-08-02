8023bed762e80f5bff61c64830e24cf46cf25d71
v.sulzhin <chijovka@gmail.com>
2020-08-02 22:45:16 +0300

    Исправление ошибки получения одного и того же eventId/transactionId 2 игроками при одновременном запросе

--- app/Models/LastEvent.php
+++ app/Models/LastEvent.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class LastEvent extends Model
 {
@@ -11,35 +12,41 @@ class LastEvent extends Model
     protected $fillable = ['eventId', 'transactionId'];
 
     public function getEventId() {
-        $row = $this::query()->first();
-
         $eventId = null;
-        if($row) {
-            $eventId = $row->eventId;
-            $eventId += 1;
-            $row->eventId = $eventId;
-            $row->save();
-        } else {
-            $eventId = 1;
-            LastEvent::create(['eventId' => $eventId, 'transactionId' => 0]);
-        }
+        DB::transaction(function () use (&$eventId) {
+            $row = $this::query()->first();
+
+            $eventId = null;
+            if ($row) {
+                $eventId = $row->eventId;
+                $eventId += 1;
+                $row->eventId = $eventId;
+                $row->save();
+            } else {
+                $eventId = 1;
+                LastEvent::create(['eventId' => $eventId, 'transactionId' => 0]);
+            }
+        });
 
         return $eventId;
     }
 
     public function getTransactionId() {
-        $row = $this::query()->first();
-
         $transactionId = null;
-        if($row) {
-            $transactionId = $row->transactionId;
-            $transactionId += 1;
-            $row->transactionId = $transactionId;
-            $row->save();
-        } else {
-            $transactionId = 1;
-            LastEvent::create(['eventId' => 0, 'transactionId' => $transactionId]);
-        }
+        DB::transaction(function () use (&$transactionId) {
+            $row = $this::query()->first();
+
+            $transactionId = null;
+            if ($row) {
+                $transactionId = $row->transactionId;
+                $transactionId += 1;
+                $row->transactionId = $transactionId;
+                $row->save();
+            } else {
+                $transactionId = 1;
+                LastEvent::create(['eventId' => 0, 'transactionId' => $transactionId]);
+            }
+        });
 
         return $transactionId;
     }
