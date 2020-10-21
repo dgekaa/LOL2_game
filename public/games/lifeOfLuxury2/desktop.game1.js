66ecec4d2bbac8a96db9057550ff314512aabe27
dgekaa <dgeka_vip@mail.ru>
2020-10-21 11:43:19 +0300

    lol2 увеличил bottomtext

--- public/games/lifeOfLuxury2/desktop.game1.js
+++ public/games/lifeOfLuxury2/desktop.game1.js
@@ -2853,7 +2853,7 @@ function game1() {
             collect_text.anchor.setTo(0.5, 0.5);
             collect_text.visible = false;
             bottomText = game.add.text(512, 609, "BONUS!", {
-                font: '100px "Arial"',
+                font: '150px "Arial"',
                 // font: '35px "Arial"',
                 fill: "#fffd6f",
                 stroke: "#000000",
