815e853cdc921806464ff022ba141d5e6df18e6b
dgekaa <dgeka_vip@mail.ru>
2020-10-21 14:10:50 +0300

    lol2 убрал anchor текста

--- public/games/lifeOfLuxury2/desktop.game1.js
+++ public/games/lifeOfLuxury2/desktop.game1.js
@@ -2853,8 +2853,7 @@ function game1() {
                 // font: '35px "Arial"',
                 fill: "#fffd6f",
                 stroke: "#000000",
-                // strokeThickness: 5,
-                strokeThickness: 20,
+                strokeThickness: 5,
                 fontWeight: 800
             });
             bottomText.anchor.setTo(0.5, 0.5);
