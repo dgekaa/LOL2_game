2f528b15202e02bf72fc331dff61cbfa2edc007a
dgekaa <dgeka_vip@mail.ru>
2020-10-21 15:02:05 +0300

    lol2 подбор пикселей для мобилки текста

--- public/games/lifeOfLuxury2/desktop.game1.js
+++ public/games/lifeOfLuxury2/desktop.game1.js
@@ -2856,7 +2856,7 @@ function game1() {
             collect_text.anchor.setTo(0.5, 0.5);
             collect_text.visible = false;
             // bottomText = game.add.text(512, 610, "BONUS!", {
-            bottomText = game.add.text(200, 610, "BONUS!", {
+            bottomText = game.add.text(512, 610, "BONUS!", {
                 font: '150px "Arial"',
                 // font: '35px "Arial"',
                 fill: "#fffd6f",
