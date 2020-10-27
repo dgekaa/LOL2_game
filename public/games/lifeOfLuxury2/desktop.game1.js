364a60d02ee0941d5506999ecd0c77a5ff3a62bf
dgekaa <dgeka_vip@mail.ru>
2020-10-27 20:29:29 +0300

    lol2 addcredits моргание

--- public/games/lifeOfLuxury2/desktop.game1.js
+++ public/games/lifeOfLuxury2/desktop.game1.js
@@ -2902,12 +2902,10 @@ function game1() {
                 if (balance + allWin < betline * lines && demo !== "demo") {
                     // !!!!!!!!!!!!!!!!!!!!!!!!!!!!1!
                     checkBalance();
-                    // if (!balanceUpdateStatus) {
-                    //     showButtons([[autoPlay, "autoPlay"]]);
-                    //     autoPlay.loadTexture("addCredit");
-                    //     addcreditFlickStatus = true;
-                    //     addCreditFlick();
-                    // }
+                    showButtons([[autoPlay, "autoPlay"]]);
+                    autoPlay.loadTexture("addCredit");
+                    addcreditFlickStatus = true;
+                    addCreditFlick();
                 }
             } else {
                 if (autostart == false) {
